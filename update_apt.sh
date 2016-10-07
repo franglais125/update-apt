9ee807f3a6fe6d24cde940da40ea0ecdfbe7d6fe
franglais125 <franglais125@gmail.com>
2016-10-07 17:11:43 -0400

    Use case-esac instead of if-elif

--- update_apt.sh
+++ update_apt.sh
@@ -1,66 +1,44 @@
 #!/bin/bash
-
 path=___BASEDIR___
 
-if   [ "$1" = "update" ]
-then
-  sudo apt update
-  exit 0
-elif [ "$1" = "upgrade" ]
-then
-  sudo apt upgrade
-  exit 0
-elif [ "$1" = "fupgrade" ]
-then
-  sudo apt full-upgrade
-  exit 0
-elif [ "$1" = "upgradable" ]
-then
-  ${path}/upgradable.sh ${path}/tmp/
-  exit 0
-elif [ "$1" = "residual" ]
-then
-  ${path}/residual.sh ${path}/tmp/
-  exit 0
-elif [ "$1" = "obsolete" ]
-then
-  ${path}/obsolete.sh ${path}/tmp/
-  exit 0
-elif [ "$1" = "new" ]
-then
-  ${path}/new.sh ${path}/tmp/
-  exit 0
-elif [ "$1" = "local" ]
-then
-  ${path}/local.sh ${path}/tmp/
-  exit 0
-elif [ "$1" = "clean" ]
-then
-  ${path}/clean.sh
-  exit 0
-elif [ "$1" = "help" ]
-then
-  ${path}/help.sh
-  exit 0
-elif [ $# -eq 0 ]
-then
-  # If no option is passed, execute the whole update sequence
-  #   Update the repo information      #
-  sudo apt update
-  #   Check for residual config files  #
-  ${path}/residual.sh ${path}/tmp/
-  #   Check for obsolete packages      #
-  ${path}/obsolete.sh ${path}/tmp/
-  #   Check for local packages         #
-  ${path}/local.sh ${path}/tmp/
-  #   Check for new packages           #
-  ${path}/new.sh ${path}/tmp/
-  #   Check for upgradable pacakges    #
-  ${path}/upgradable.sh ${path}/tmp/
-  exit 0
-else
-  echo "Error: \"$1\" is not a valid command"
-  exit 1
-fi
+case "$1" in
+  "update")
+    sudo apt update ;;
+  "upgrade")
+    sudo apt upgrade ;;
+  "fupgrade")
+    sudo apt full-upgrade ;;
+  "upgradable")
+    ${path}/upgradable.sh ${path}/tmp/ ;;
+  "residual")
+    ${path}/residual.sh ${path}/tmp/ ;;
+  "obsolete")
+    ${path}/obsolete.sh ${path}/tmp/ ;;
+  "new")
+    ${path}/new.sh ${path}/tmp/ ;;
+  "local")
+    ${path}/local.sh ${path}/tmp/ ;;
+  "clean")
+    ${path}/clean.sh ;;
+  "help")
+    ${path}/help.sh ;;
+  "")
+    # If no option is passed, execute the whole update sequence
+    #   Update the repo information      #
+    sudo apt update
+    #   Check for residual config files  #
+    ${path}/residual.sh ${path}/tmp/
+    #   Check for obsolete packages      #
+    ${path}/obsolete.sh ${path}/tmp/
+    #   Check for local packages         #
+    ${path}/local.sh ${path}/tmp/
+    #   Check for new packages           #
+    ${path}/new.sh ${path}/tmp/
+    #   Check for upgradable pacakges    #
+    ${path}/upgradable.sh ${path}/tmp/ ;;
+  *)
+    echo "Error: \"$1\" is not a valid command"
+    exit 1 ;;
+esac
 
-
+exit 0
